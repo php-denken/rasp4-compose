004c4a1b7426f0231413e69dadb4c00370c7fd02
php-denken <sebastian@php-denken.de>
2021-03-10 21:28:37 +0100

    replace atomatically variable parts in docker compose template with configured variables

--- pull-replace.sh
+++ pull-replace.sh
@@ -3,7 +3,12 @@ set -ex
 
 source config.cfg
 
+PLACEHOLDER_ARRAY=("RASPBERRY_HOST_IP" "NEXTCLOUD_DB_PW" "NEXTCLOUD_DB_ROOT_PASSWORD" "NEXTCLOUD_DB_USER" "NEXTCLOUD_DB_NAME")
+
 git pull
 cp -f docker-compose.yml.template docker-compose.yml
 
-sed -i "s/###RASPBERRY_HOST_IP###/$RASPBERRY_HOST_IP/" docker-compose.yml
+for PLACEHOLDER in ${PLACEHOLDER_ARRAY[*]}
+do
+    sed -i "s/###$PLACEHOLDER###/${!PLACEHOLDER}/" docker-compose.yml
+done
